a54a7c3e7a6686acbf22f37378ca27fc80243a8f
KapustinKirill <103725111+KapustinKirill@users.noreply.github.com>
2022-08-19 15:21:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 BOT_TOKEN = "5611876177:AAFEIG1ePVi5PzMjgMZwNunb_u5lbKHTIa8"
 APP_URL = "https://dhawktestapplication.herokuapp.com/" +BOT_TOKEN
+DB_URI = "postgres://tjwycdcqmhotok:e957b5593222ea8af40fe594d639775daec4ed46c51e34819abc6c4a74debfa9@ec2-54-229-217-195.eu-west-1.compute.amazonaws.com:5432/d864flhgj9d9at"
