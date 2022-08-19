645d14e13e8f42caa983b8389ba59841acdc2e3f
KapustinKirill <103725111+KapustinKirill@users.noreply.github.com>
2022-08-19 15:21:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,52 @@ import telebot
 import logging
 from config import *
 from flask import Flask, request
+import psycopg2
 
 bot = telebot.TeleBot(BOT_TOKEN)
 server = Flask(__name__)
 logger = telebot.logger
 logger.setLevel(logging.DEBUG)
 
+db_connection = psycopg2.connect(DB_URI, sslmode="require")
+db_object = db_connection.cursor()
+
 @bot.message_handler(commands=["start"])
 def start(message):
+    user_id = message.from_user.id
     username = message.from_user.username
-    bot.reply_to(message, f"Hello {username}")
+    bot.reply_to(message, f"Hello, {username}!")
+
+    db_object.execute(f"SELECT id FROM users WHERE id = {user_id}")
+    result = db_object.fetchone()
+
+    if not result:
+        db_object.execute("INSERT INTO users(id, username, messages) VALUES (%s, %s, %s)", (user_id, username, 0))
+        db_connection.commit()
+
+    update_messages_count(user_id)
+
 
 @bot.message_handler(commands=["stats"])
-def stats(message):
+def get_stats(message):
+    db_object.execute("SELECT * FROM users ORDER BY messages DESC LIMIT 10")
+    result = db_object.fetchall()
+
+    if not result:
+        bot.reply_to(message, "No data...")
+    else:
+        reply_message = "- Top flooders:\n"
+        for i, item in enumerate(result):
+            reply_message += f"[{i + 1}] {item[1].strip()} ({item[0]}) : {item[2]} messages.\n"
+        bot.reply_to(message, reply_message)
+
+    update_messages_count(message.from_user.id)
+
+
+@bot.message_handler(func=lambda message: True, content_types=["text"])
+def message_from_user(message):
     user_id = message.from_user.id
-    bot.reply_to(message, f"You ID: {user_id}")
+    update_messages_count(user_id)
 
 
 @server.route(f"/{BOT_TOKEN}", methods = ["POST"])
