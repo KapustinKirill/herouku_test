7e0dc224a0d2a689be9120f9d0868c7cad243924
KapustinKirill <103725111+KapustinKirill@users.noreply.github.com>
2022-08-21 23:28:06 +0300

    deploy

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def get_vsesmart(message):
     try:
         parsing_vse_smart(bot, message)
     except Exception as ex:
-        bot.send_message(message.from_user.id, ex)
+        print(ex)
     update_messages_count(message.from_user.id)
 
 def search_count(category: str, message):
