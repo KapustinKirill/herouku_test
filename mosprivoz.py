9881cdf85056b7c373ba9814a58a6aac3a7334ed
KapustinKirill <103725111+KapustinKirill@users.noreply.github.com>
2022-08-23 13:56:05 +0300

    deploy

--- mosprivoz.py
+++ mosprivoz.py
@@ -73,11 +73,15 @@ def parsing_data():
 #DB_URI = "postgres://tjwycdcqmhotok:e957b5593222ea8af40fe594d639775daec4ed46c51e34819abc6c4a74debfa9@ec2-54-229-217-195.eu-west-1.compute.amazonaws.com:5432/d864flhgj9d9at"
 
 def data_post_to_base(items):
-    host = "ec2-54-229-217-195.eu-west-1.compute.amazonaws.com"
-    database="d864flhgj9d9at"
-    user = "tjwycdcqmhotok"
+    #host = "ec2-54-229-217-195.eu-west-1.compute.amazonaws.com"
+    host = os.getenv('host')
+    #database="d864flhgj9d9at"
+    database = os.getenv('database')
+    #user = "tjwycdcqmhotok"
+    user = os.getenv('user')
     port = "5432"
-    password = "e957b5593222ea8af40fe594d639775daec4ed46c51e34819abc6c4a74debfa9"
+    #password = "e957b5593222ea8af40fe594d639775daec4ed46c51e34819abc6c4a74debfa9"
+    password = os.getenv('password')
     engine = sqlalchemy.create_engine(f"postgresql+psycopg2://{user}:{password}@{host}/{database}")
     engine.connect()
     parsing_moment = datetime.now()  # Записываем момент времени обработки
